d58b812720ab5092222954b83b0be3e7aeee953d
Abid004 <92315375+Abid004@users.noreply.github.com>
2021-12-14 03:54:53 +0100

    index

--- function.php
+++ function.php
@@ -25,3 +25,112 @@ function deleteItem($index) {
     
 }
 ?>
+<?php
+if(isset($_GET["choice"])) {
+    $stole=$_GET["choice"];
+    $c= count($stole);
+    $price= 0.0;
+
+    for($i=0;$i<$c;$i++) {
+        if($stole[$i]==8900){
+            $price=$price+14.995;
+            echo "du har bestilt Nobel 8900<br>"; 
+        }
+        if($stole[$i]==8920){
+            $price=$price+5.595;
+            echo "du har bestilt Nobel 8920<br>"; 
+        } 
+        if($stole[$i]==6360){
+            $price=$price+27.545 ;
+            echo "du har bestilt Munin 6360<br>"; 
+        }
+        if($stole[$i]==6510){
+            $price=$price+17.450;
+            echo "du har bestilt Bear R 6510<br>"; 
+        }
+        if($stole[$i]==6010){
+            $price=$price+14.445;
+            echo "du har bestilt Cantate 6010<br>"; 
+        }
+        if($stole[$i]==5900){
+            $price=$price+11.260;
+            echo "du har bestilt Plus 5900<br>"; 
+        }
+        if($stole[$i]==5980){
+            $price=$price+15.550;
+            echo "du har bestilt Plus 5980<br>"; 
+        }
+        if($stole[$i]==5060){
+            $price=$price+17.110;
+            echo "du har bestilt Plus 5060<br>"; 
+        }
+        if($stole[$i]==5197){
+            $price=$price+11.480 ;
+            echo "du har bestilt Casa 5197<br>"; 
+        }
+        if($stole[$i]==7900){
+            $price=$price+11.260;
+            echo "du har bestilt Edge 7900<br>"; 
+        }
+        if($stole[$i]==5411) {
+            $price=$price+9.975;
+            echo "du har bestilt Scala 5411<br>"; 
+        }
+        if($stole[$i]==5391){
+            $price=$price+8.965;
+            echo "du har bestilt Senator 5390 & 5391<br>"; 
+        }
+        if($stole[$i]==182){
+            $price=$price+5180;
+            echo "du har bestilt Armstol 182<br>"; 
+        }
+        if($stole[$i]==183){
+            $price=$price+5.760;
+            echo "du har bestilt Gyngestol 183<br>"; 
+        }
+        if($stole[$i]==5040){
+            $price=$price+32.660 ;
+            echo "du har bestilt 5040 Multiplus<br>"; 
+        }
+        if($stole[$i]==6311){
+            $price=$price+23.270;
+            echo "du har bestilt Home-Line 6311<br>"; 
+        }
+        if($stole[$i]==6341){
+            $price=$price+27.700;
+            echo "du har bestilt Home-Line 6341<br>"; 
+        }
+        if($stole[$i]==5463){
+            $price=$price+12.810 ;
+            echo "du har bestilt Fastoflex 5463<br>"; 
+        }
+        if($stole[$i]==2550 ){
+            $price=$price+4.370;
+            echo "du har bestilt 2550 Stag W<br>"; 
+        }
+        if($stole[$i]==2500){
+            $price=$price+4.215;
+            echo "du har bestilt 2500 Stag S<br>"; 
+        }
+        if($stole[$i]==210){
+            $price=$price+3.320;
+            echo "du har bestiltNo. 210r<br>"; 
+        }
+        if($stole[$i]== 2411){
+            $price=$price+4.420;
+            echo "du har bestilt Scala 2411<br>"; 
+        }
+        if($stole[$i]==182){
+            $price=$price+5.180;
+            echo "du har bestilt Armstol 182<br>"; 
+        }
+
+
+    }
+    echo "Total Price is: " . $price. "<br>";
+}
+
+else{
+
+    echo "kurvn er tømt venlig valg vare";
+}
--- index.php
+++ index.php
@@ -15,6 +15,48 @@ include("./functionall.php");
    <title>Prototype(Farstrup Furniture A/S)</title>
    <link rel = "stylesheet" href="style.css">
 </head>
+<body>
+  <h1> add what you want to by</h1>
+  <h2> put the product number if you want to add to cart</h2>
+    <form action= "function.php" method="get"/>
+    <input type = "Lænerstole" name = "choice[]" eksist = "8900"/>Nobel 8900, Price: 14.995 Kr. <br/>
+    <input type = "Lænerstole" name = "choice[]" eksist = "8920"/>Nobel 8920, Price:15.595 Kr. <br/>
+    <input type = "Lænerstole" name = "choice[]" eksist = "6360"/>Munin 6360, Price: 27.545 Kr. <br/>
+    <input type = "Lænerstole" name = "choice[]" eksist = "6510"/>Bear R 6510, Price: 17.450 Kr. <br/>
+    <input type = "Lænerstole" name = "choice[]" eksist = "6010"/>Cantate 6010, Price:14.445 Kr. <br/>
+    <input type = "Lænerstole" name = "choice[]" eksist = "5900"/>Plus 5900, Price:11.260 Kr. <br/>
+    <input type = "Lænerstole" name = "choice[]" eksist = "5980"/>Plus 5980, Price:15.550 Kr. <br/>
+    <input type = "Lænerstole" name = "choice[]" eksist = "5060"/>Plus 5060, Price:17.110 Kr. <br/>
+    <input type = "Lænerstole" name = "choice[]" eksist = "5197"/>Casa 5197, Price:11.480 Kr. <br/>
+    <input type = "Lænerstole" name = "choice[]" eksist = "7900"/>Edge 7900, Price: 11.260 Kr. <br/>
+    <input type = "Lænerstole" name = "choice[]" eksist = "5411"/>Scala 5411, Price:9.975 Kr. <br/>
+    <input type = "Lænerstole" name = "choice[]" eksist = "5391"/>Senator 5390 & 5391, Price: 8.965 Kr. <br/>
+    <input type = "Lænerstole" name = "choice[]" eksist = "182"/>Armstol 182, Price: 5180 Kr. <br/>
+    <input type = "Lænerstole" name = "choice[]" eksist = "183"/>Gyngestol 183, Price:5.760 Kr. <br/>
+    <input type = "Lænerstole" name = "choice[]" eksist = "5040"/>5040 Multiplus, Price:32.660 Kr. <br/>
+    <input type = "Lænerstole" name = "choice[]" eksist = "6311"/> Home-Line 6311, Price: 23.270 Kr. <br/>
+    <input type = "Lænerstole" name = "choice[]" eksist = "6341"/>Home-Line 6341, Price:27.700 Kr. <br/>
+    <input type = "Lænerstole" name = "choice[]" eksist = "5463"/>Fastoflex 5463, Price:12.810 Kr. <br/>
+    <input type = "Lænerstole" name = "choice[]" eksist = "2550"/> 2550 Stag W, Price:4.370 Kr. <br/>
+    <input type = "Lænerstole" name = "choice[]" eksist = "2500 "/>2500 Stag S, Price: 4.215 Kr. <br/>
+    <input type = "Lænerstole" name = "choice[]" eksist = "210"/>No. 210r, Price:  3.320 Kr. <br/>
+    <input type = "Lænerstole" name = "choice[]" eksist = "2411"/>Scala 2411, Price: 4.420Kr. <br/>
+    <input type = "Lænerstole" name = "choice[]" eksist = "182"/>Armstol 182, Price:5180Kr. <br/> 
+    <input type="submit" eksist="order"/>
+</form>
+</body>
+<body bgcolor="#FF00000">
+    <form method="post">
+        <input type="text" name="product">
+        <butto type="submit">Add coment</button>
+        <form>
+    <ul>
+      <?php foreach(getFromFile() as $i => $product) { ?>
+          <li><?php echo $product;?>
+       <a href="?i=<?php echo $i; ?>">delete</a>
+       </li>
+          <?php } print_r($_GET);
+          ?>
 <style>
   p { 
     text-align: center;
@@ -22,7 +64,7 @@ include("./functionall.php");
   img { 
    
     position: relative;
-    top: -5100px;
+    top: -5700px;
     width: 170px;
     border-radius: 40PX;
     float: right;
@@ -31,7 +73,7 @@ include("./functionall.php");
   </style>
   <div class= "container">
     <div class= "glass">
- <img src="images/pp.jpeg", style = "top:-5100px">
+ <img src="images/pp.jpeg", style = "top:-5700px">
 </div>
 <div class= "container">
     <div class= "glass">
@@ -140,25 +182,7 @@ include("./functionall.php");
  <img src="images/Senator5373.jpeg",>
 </div>
 </head>
-<body bgcolor="#FF00000">
-    <form method="post">
-        <input type="text" name="product">
-        <butto type="submit">Add</button>
-
-    <form>
-
-
     <ul>
-
-       <?php foreach(getFromFile() as $i => $product) { ?>
-          <li><?php echo $product;?>
-       <a href="?i=<?php echo $i; ?>">delete</a>
-       </li>
-          <?php } print_r($_GET);
-          ?>
-     
-     
-     <ul>
  </body>
  </html>
 
